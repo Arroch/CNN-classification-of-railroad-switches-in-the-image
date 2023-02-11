ae3427983f78026577cd0be15008cf1c13be59de
Roman Breus <breus134@gmail.com>
2023-02-11 15:38:08 +0400

    deleted wrong rows(comments rows)

--- sort_img.py
+++ sort_img.py
@@ -23,15 +23,8 @@ def sort_img():
                             if (xmax - xmin) * (ymax - ymin) >= 2048 * 1084 * 0.05:
 
                                 i+=1
-                                #if not os.path.isdir(os.path.join("large_im", path[4 :])[: 38]):
-                                    #os.makedirs(os.path.join("large_im", path[4 :])[: 38])
-                                #if not os.path.isdir(os.path.join("large_im", img_path[4:])[: 38]):
-                                    #os.makedirs(os.path.join("large_im", img_path[4:])[: 38])
-                                #os.replace(img_path,os.path.join("large_im", img_path[4 :]))
                                 image_path = cv2.imread(img_path)
                                 cv2.imwrite("large_im/img" + str(i) + '.png', image_path)
-                                #with open(os.path.join("large_im", path[4 :]), "w") as write_file:
-                                    #json.dump(data, write_file, indent=4)
                                 print("PATH", os.path.join(roots[2:], file))
 
 
